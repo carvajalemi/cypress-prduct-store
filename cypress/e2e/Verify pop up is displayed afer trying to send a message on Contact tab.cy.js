419203ff05cf8fb6d8538f0c171af23c35cfe652
Emilia Carvajal <emilia.carvajalm@gmail.com>
2023-01-16 13:09:13 -0400

    Update Verify pop up is displayed afer trying to send a message on Contact tab.cy.js

--- cypress/e2e/Verify pop up is displayed afer trying to send a message on Contact tab.cy.js
+++ cypress/e2e/Verify pop up is displayed afer trying to send a message on Contact tab.cy.js
@@ -1,5 +1,25 @@
-describe('empty spec', () => {
-  it('passes', () => {
-    cy.visit('https://example.cypress.io')
+describe('Test pop up is displayed after Sending a message on Cntact tab', () => {
+  it('Should validate the pop up is displayed after Sending a message on Cntact tab', () => {
+    cy.visit('https://www.demoblaze.com/index.html')
+  
+
+  //Verify the Contact tab is displayed and click over it
+  cy.get ('[data-target="#exampleModal"]').click();
+
+  //Verify the pop up is visible
+  cy.contains('Contact Email').should("be.visible");
+  
+  //Verify the content of the pop up contains the text New message
+  cy.get('h5').contains('New message');
+  
+  //Verify the the pop up contains the Send Message button and click over it
+  cy.get('[onclick="send()"]').click();
+ 
+  //Checking the window alert text
+  cy.on('window:alert',(txt)=>{
+    
+  //Assertion
+  expect(txt).to.contains('Thanks for the message!!');
   })
+})
 })
