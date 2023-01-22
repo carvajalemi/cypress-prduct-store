58ebabe45dc7147cbab4f3e6200af3325e4a3398
Emilia Carvajal <emilia.carvajalm@gmail.com>
2023-01-22 19:19:25 -0400

    Update Carouse-Verify Previous arrow in the carousel items is working correctly.cy.cy.js

--- cypress/e2e/Carouse-Verify Previous arrow in the carousel items is working correctly.cy.cy.js
+++ cypress/e2e/Carouse-Verify Previous arrow in the carousel items is working correctly.cy.cy.js
@@ -1,5 +1,5 @@
 describe('Test Previous arrow in the carousel items is working correctly', () => {
-  it('Should validate Prevous arrow in the carousel items is working correctly', () => {
+  it('Should validate Previous arrow in the carousel items is working correctly', () => {
     cy.visit('https://www.demoblaze.com/index.html')
 
   //Verify Carousel is displayed
